74e0d8062bd0eed1e83cf4a3b43f4e8bd5822548
Anita <aneeta.ashraf7@gmail.com>
2024-01-05 20:43:32 -0500

    added logo to the nav bar

--- src/components/Cakecards.jsx
+++ src/components/Cakecards.jsx
@@ -5,39 +5,42 @@ import Card from 'react-bootstrap/Card';
 
 export default function Cakecards() {
     return (
-        <div>
-            <div className="cards">
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="./images/cake2.jpg" />
-                    <Card.Body>
-                        <Card.Title>Chocolate Cake</Card.Title>
-                        <Card.Text>
-                            Soft chocolate base, Mocca Filling, Covered with Chocolate Genache.
-                        </Card.Text>
-                        <Button variant="primary">Price</Button>
-                    </Card.Body>
-                </Card>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="./images/cake.jpg" />
-                    <Card.Body>
-                        <Card.Title>Wedding Cake</Card.Title>
-                        <Card.Text>
-                            Soft chocolate base, Mocca Filling, Covered with Chocolate Genache.
-                        </Card.Text>
-                        <Button variant="primary">Price</Button>
-                    </Card.Body>
-                </Card>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="./images/cake3.jpg" />
-                    <Card.Body>
-                        <Card.Title>Party Cake</Card.Title>
-                        <Card.Text>
-                            Soft chocolate base, Mocca Filling, Covered with Chocolate Genache.
-                        </Card.Text>
-                        <Button variant="primary">Price</Button>
-                    </Card.Body>
-                </Card>
-            </div>
-        </div>
+        <div className='kakku'>
+
+            <Card className="cards" style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="./images/cake2.jpg" alt="" width="500" height="400"/>
+                <Card.Body>
+                    <Card.Title>Chocolate Cake</Card.Title>
+                    <Card.Text>
+                        Soft chocolate base, Mocca Filling, Covered with Chocolate Genache.
+                    </Card.Text>
+                    <Button className="colorbutton">Price</Button>
+                </Card.Body>
+            </Card>
+
+            <Card className="cards" style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="./images/cake.jpg" alt="" width="500" height="400" />
+                <Card.Body>
+                    <Card.Title>Wedding Cake</Card.Title>
+                    <Card.Text>
+                        Soft chocolate base, Mocca Filling, Covered with Chocolate Genache.
+                    </Card.Text>
+                    <Button className="colorbutton">Price</Button>
+                </Card.Body>
+            </Card>
+
+            <Card className="cards" style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="./images/cake3.jpg" alt="" width="500" height="400" />
+                <Card.Body>
+                    <Card.Title>Party Cake</Card.Title>
+                    <Card.Text>
+                        Soft chocolate base, Mocca Filling, Covered with Chocolate Genache.
+                    </Card.Text>
+                    <Button className="colorbutton">Price</Button>
+                </Card.Body>
+            </Card>
+            
+
+        </div >
     );
 }
