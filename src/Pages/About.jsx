78fec171071c93ea8f570d0e800bf3637b7dd3af
Anita <aneeta.ashraf7@gmail.com>
2024-02-07 23:53:44 -0500

    Contact page updated

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,16 +1,21 @@
 import Navbarpink from "../components/Navbarpink";
 
-export default function About(){
-    return(
+export default function About() {
+    return (
         <div>
-<Navbarpink/>
-<h6 className="aboutheading">About Us</h6>
-<br/>
-<p className="about">Welcome to <strong>Anita's Bakery</strong> Online! 
-We're all about bringing the bakery experience to you, wherever you are. 
-Our virtual shelves are stocked with delicious treats, from cookies to cakes, made fresh just for you. With easy online ordering, we're here to satisfy your sweet cravings without any hassle. Treat yourself or surprise someone special with a box of our homemade goodies. It's simple, it's sweet, and it's just a click away. Welcome to the world of <strong>Anita's Bakery</strong> 
-     Online – where the joy of baking meets the ease of ordering!</p>
+            <Navbarpink />
+            <h6 className="bakery">About Us</h6>
+            <br />
+            <p className="about">Welcome to <strong>Anita's Bakery</strong> Online!
+                We're all about bringing the bakery experience to you, wherever you are.
+                Our virtual shelves are stocked with delicious treats, from cookies to cakes, made fresh just for you. With easy online ordering, we're here to satisfy your sweet cravings without any hassle. Treat yourself or surprise someone special with a box of our homemade goodies. It's simple, it's sweet, and it's just a click away. Welcome to the world of <strong>Anita's Bakery</strong>
+                Online – where the joy of baking meets the ease of ordering!</p>
+            <br />
+            <div>
+                <h3 className="bakery2">We Bake with love and Passion</h3>
+            </div>
 
-</div>
+        </div>
+
     );
 }
--- src/components/Cakecards.jsx
+++ src/components/Cakecards.jsx
@@ -40,6 +40,7 @@ export default function Cakecards() {
                 </Card.Body>
             </Card>
             
+            
 
         </div >
     );
