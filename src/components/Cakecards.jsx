a83c2d46860646e19bed0dd69bd0f71c5881355f
Anita <aneeta.ashraf7@gmail.com>
2024-02-29 21:59:44 -0500

    added more cakes

--- src/components/Cakecards.jsx
+++ src/components/Cakecards.jsx
@@ -38,6 +38,39 @@ export default function Cakecards() {
                     <a href="/" class="btn btn-primary">Price</a>
                 </Card.Body>
             </Card>
+
+            <Card className="cards" style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="./images/cake5.jpg" alt="" />
+                <Card.Body>
+                    <Card.Title>Wedding Cake</Card.Title>
+                    <Card.Text>
+                        Red velvet, Cream Cheese Filling, Decorated with fondant.
+                    </Card.Text>
+                    <a href="/" class="btn btn-primary">Price</a>
+                </Card.Body>
+            </Card>
+
+            <Card className="cards" style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="./images/cake13.jpeg" alt="" />
+                <Card.Body>
+                    <Card.Title>Party Cake</Card.Title>
+                    <Card.Text>
+                        Soft chocolate base, Mocca Filling, Covered with Chocolate Genache.
+                    </Card.Text>
+                    <a href="/" class="btn btn-primary">Price</a>
+                </Card.Body>
+            </Card>
+
+            <Card className="cards" style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="./images/cake4.jpg" alt="" />
+                <Card.Body>
+                    <Card.Title>Theme Cake</Card.Title>
+                    <Card.Text>
+                        Vanilla Cake, Decorated with fondant.
+                    </Card.Text>
+                    <a href="/" class="btn btn-primary">Price</a>
+                </Card.Body>
+            </Card>
             
             
 
